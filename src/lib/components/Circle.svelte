c8aea7979b5eb3d20033d2fc3a1296f020352aa8
pnm122 <pnmartin02@gmail.com>
2024-08-13 23:01:32 -0400

    Add Circle component

---
+++ src/lib/components/Circle.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import generateRandomColor from "$lib/utils/generateRandomColor";
+
+  interface Props {
+    class?: string
+    style?: string
+    children?: () => any
+    /** @default 0.001 */
+    density?: number
+    /** @default 0.1 */
+    friction?: number
+    /** @default 0.01 */
+    frictionAir?: number
+    /** @default false */
+    isStatic?: boolean
+    /** @default 0 */
+    restitution?: number
+    /** @default 0.5 */
+    frictionStatic?: number
+    /** @default 0 */
+    radius?: number
+    /** Whether to generate a random background
+     * @default false
+     */
+    randomBackground?: boolean
+  }
+
+  const {
+    class: classname,
+    style,
+    children,
+    density = 0.001,
+    friction = 0.1,
+    frictionAir = 0.01,
+    isStatic = false,
+    restitution = 0,
+    frictionStatic = 0.5,
+    randomBackground = false
+  }: Props = $props()
+</script>
+
+<div
+  data-shape="circle"
+  data-density={density}
+  data-friction={friction}
+  data-friction-air={frictionAir}
+  data-is-static={isStatic}
+  data-restitution={restitution}
+  data-friction-static={frictionStatic}
+  class="circle {classname}"
+  style="{style} {randomBackground ? `background-color: ${generateRandomColor()}` : ''}">
+  {#if children}
+    {@render children()}
+  {/if}
+</div>
+
+<style lang="scss">
+  .circle {
+    aspect-ratio: 1;
+    overflow: hidden;
+    border-radius: 999px;
+  }
+</style>
---
+++ src/lib/utils/generateRandomColor.ts
@@ -0,0 +1,6 @@
+export default function generateRandomColor() {
+  function random(min: number, max: number) {
+    return ((Math.random() * (max - min)) + min).toFixed(2)
+  }
+  return `hsl(${random(0, 360)} 75 ${random(33, 67)})`
+}
